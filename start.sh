a9c8c3e9ffb30d222c1a92f717b26341d48e0952
Henry Mohn <hmohniii@gmail.com>
2019-02-11 14:27:32 -0600

    Updated to check for newer server

--- start.sh
+++ start.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-if [ ! -f /data/bedrock_server ]; then
+md5sum -c /data/md5check --quiet
+RESULT=$?
+
+if [ $RESULT -ne 0 ]; then
 	echo "Extracting server..."
 	unzip /opt/bedrock_server.zip -d /data
+	md5sum /opt/bedrock_server.zip > /data/md5check
 fi
 
 cd /data
